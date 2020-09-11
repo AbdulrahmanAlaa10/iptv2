68c08fc52dd3cd4ae4cf1e5bc40b7a5a5ffe54b1
AbdulrahmanAlaa10 <58831432+AbdulrahmanAlaa10@users.noreply.github.com>
2020-09-11 21:52:13 +0200

    1

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,170 +7,165 @@ function Home() {
   return (
 
     <Router>
-
     <header class="header">
         <div class="header1">
-                <h1 className="hhh">🎀 MORE THAN 2 MILLION CALLER 🎀</h1>
-                <p class="Quiz">✔ Our site is distinguished by Fast navigation between channels, Easy subscription and activation, Put a favorite channel on your desired, High quality 4K,FHD,HD,SD,LOW Combines all channels packagesn, 24-hour updatesh, Supports Arabic, Allows you to turn on a single subscription on all your devices, There is a library of films and serials Arab and foreign countless,There are 4K Movies, 24 hours free trial before buy. <br /> 📺 Why are you waiting? Subscribe Now 📺</p>
+          <div className="container">
+                <h1 className="hhh">🎀 THE BEST IPTV SUBSCRIPTION SERVICE FOR ANY DEVICE, FROM ANY LOCATION🎀</h1>
+                <p class="Quiz">✔ Our SERVICES is distinguished by Fast navigation between channels, Easy subscription and activation, Put a favorite channel on your desired, High quality 4K,FHD,HD Combines all channels packages, 24-hour updatesh, There is a library of films and foreign countless,There are 4K Movies, 24 hours free trial before buy. <br /> 📺 Why are you waiting? Subscribe Now 📺</p>
                 <a href="/Paid">
                     <button className="btn bttn btn-outline-danger">
                        ChannelPaid
                     </button>
                 </a>
+                </div>
         </div>
     </header>
 
-
     <section className="informtion">
+      <div className="infoooor">
       <div className="container">
-
           <div className="inforh1 col-md-12 col-lg-12 col-sm-12 col-xs-12">
             <h2 className="inforh2">↪ INFORMATION ABOUT OUR SERVICES ↩</h2>
             <h3 className="inforh3">💯 HIGH QUALITY 💯</h3>
-            <p className="inforp">✔ 4K,FHD,HD,SD,LOW shows for ultimate playback satisfaction، You can watch your favorite program.</p>
+            <p className="inforp">✔ 4K,FHD,HD shows for ultimate playback satisfaction، You can watch your favorite program.</p>
 
             <h2 className="inforhh2">📺 WATCHING NOW 📺</h2>
-            <p className="inforp1">✔ More than +7000 channels and +2000 vods in one Subscription and in one place and Many channels are distinctive in your hands and prices very cheap.</p>
+            <p className="inforp1">✔ More than +10000 channels and +105000 vods in one Subscription and in one place and Many channels are distinctive in your hands and prices very cheap.</p>
 
             <h2 className="inforhh3">🥇 OTHER ADVANTAGES 🥇</h2>
-            <p className="inforp2">✔ Fast navigation between channels, Easy subscription and activation, Put a favorite channel on your desired, Combines all channels packagesn, 24-hour updatesh, Supports Arabic, Allows you to turn on a single subscription on all your devices, 24 hours free trial before buy.</p>
-
-            <h2 className="inforhh3">🥇 OTHER ADVANTAGES 🥇</h2>
-            <p className="inforp2">✔ Fast navigation between channels, Easy subscription and activation, Put a favorite channel on your desired, Combines all channels packagesn, 24-hour updatesh, Supports Arabic, Allows you to turn on a single subscription on all your devices, 24 hours free trial before buy.</p>
+            <p className="inforp2">✔ Fast navigation between channels, Easy subscription and activation, Put a favorite channel on your desired, Combines all channels packages, 24-hour updatesh, 24 hours free trial before buy.</p>
 
             <h2 className="inforhh4">⚠️ Attention ⚠️</h2>
             <p className="inforp3">✔ There are 4K movies , No channels 🔞.</p>
 
-            <h2 className="inforhh5">💰 MULTI PAYMENT METHODS 💰</h2>
-            <p className="inforp4">✔ Paypal , Western Union , Skrill , Neteller , vodafone cash , مكينة فوري , البنك العربي , البنك الراجحى , بنك الإنماء .</p>
-
-
+            <h2 className="inforhh5">💰 PAYMENT METHODS 💰</h2>
+            <p className="inforp4">✔ Paypal باي بال</p>
             </div>
+          </div>
           </div>
     </section>
 
 
 
     <section className="informtion2">
+      <div className="infoor">
       <div className="container">
 
       <h2 className="inforhh6">↪ Why you choose our services? See what's special about our services? ↩</h2>
-      <p className="inforp5">✔ Because Millions of happy customers keep coming back due to the high quality of our iptv service. We pride in top quality technical support and we are the most powerful IPTV provider,Our data center utilises the latest technology, giving you fastest speeds on the market, Now you can watching more than +7000 channels and +2000 vods in one Subscription and in one place and Many channels and prices very cheap, We have it Subscription for 3 MONTHS, 6 MONTHS, 12 MONTHS and prices very cheap, Why stop? BUY NOW.</p>
+      <p className="inforp5">✔ Because Millions of happy customers keep coming back due to the high quality of our iptv service. We pride in top quality technical support and we are the most powerful IPTV provider,Our data center utilises the latest technology, giving you fastest speeds on the market, Now you can watching more than +10000 channels and +105000 vods in one Subscription and in one place and Many channels and prices very cheap, We have it Subscription for 3 MONTHS, 6 MONTHS, 12 MONTHS and prices very cheap, Why stop? BUY NOW.</p>
 
 
-   
-          </div>
+      </div>
+      </div>
     </section>
 
 
     <section className="sece text-center">
             <div className="hhhhhh">
+
+
               <h1 className="hshow">
               🤗 SUBSCRIPTIONS 🤗
               </h1>
-            </div>
-      
-            <div className="container">
+              <div className="container">
               <div className="row">
+
       
                 <div className="col-md-3 col-lg-3 col-xs-12 col-sm-12">
                   <div className="box">
                   <h1>24 HOURS</h1>
-                  <p>0$</p>
+                  <p>10€</p>
                   <ul className="seceul list-unstyled">
                   <li>Pay as You go</li>
-                  <li>5000 channels</li>
-                  <li>2000 vods (AR, non)</li>
-                  <li>4k, FHD.HD,SO Quality</li>
+                  <li>4k,FHD.HD, Quality</li>
                   <li>NO SETUPS REQUIRED</li>
+                  <li>+105000 voods (AR, non)</li>
                   <li>ALL IPTV RECIVERS</li>
+                  <li>+10000 channels</li>
                   <li>99.9% Uptime</li>
                   </ul>
                   <br />
                   <h3 className="paypay">send to Paypal: med.benzbair@gmail.com</h3>
-                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-5 fa-paypal"></i>Pay Now</button></a>
+                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-paypal"></i>Pay Now</button></a>
 
                   </div>
                 </div>
                 <div className="col-md-3 col-lg-3 col-xs-12 col-sm-12">
                 <div className="box">
                   <h1>3 MONTHS</h1>
-                  <p>15$</p>
+                  <p>20€</p>
                   <ul className="seceul list-unstyled">
                   <li>Pay as You go</li>
-                  <li>5000 channels</li>
-                  <li>2000 vods (AR, non)</li>
-                  <li>4k, FHD.HD,SO Quality</li>
+                  <li>4k,FHD.HD, Quality</li>
                   <li>NO SETUPS REQUIRED</li>
+                  <li>+105000 voods (AR, non)</li>
                   <li>ALL IPTV RECIVERS</li>
+                  <li>+10000 channels</li>
                   <li>99.9% Uptime</li>
                   </ul>
                   <br />
                   <h3 className="paypay">send to Paypal: med.benzbair@gmail.com</h3>
-                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-5 fa-paypal"></i>Pay Now</button></a>
+                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-paypal"></i>Pay Now</button></a>
                   </div>
                 </div>
                 <div className="col-md-3 col-lg-3 col-xs-12 col-sm-12">
                 <div className="box">
                   <h1>6 MONTHS</h1>
-                  <p>25$</p>
+                  <p>30€</p>
                   <ul className="seceul list-unstyled">
                   <li>Pay as You go</li>
-                  <li>5000 channels</li>
-                  <li>2000 vods (AR, non)</li>
-                  <li>4k, FHD.HD,SO Quality</li>
+                  <li>4k,FHD.HD, Quality</li>
                   <li>NO SETUPS REQUIRED</li>
+                  <li>+105000 voods (AR, non)</li>
                   <li>ALL IPTV RECIVERS</li>
+                  <li>+10000 channels</li>
                   <li>99.9% Uptime</li>
                   </ul>
                   <br />
                   <h3 className="paypay">send to Paypal: med.benzbair@gmail.com</h3>
-                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-5 fa-paypal"></i>Pay Now</button></a>
+                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-paypal"></i>Pay Now</button></a>
                   </div>
                 </div>
                 <div className="col-md-3 col-lg-3 col-xs-12 col-sm-12">
                 <div className="box">
                   <h1>12 MONTHS</h1>
-                  <p>40$</p>
+                  <p>60€</p>
                   <ul className="seceul list-unstyled">
                   <li>Pay as You go</li>
-                  <li>5000 channels</li>
-                  <li>2000 vods (AR, non)</li>
-                  <li>4k, FHD.HD,SO Quality</li>
+                  <li>4k,FHD.HD, Quality</li>
                   <li>NO SETUPS REQUIRED</li>
+                  <li>+105000 voods (AR, non)</li>
                   <li>ALL IPTV RECIVERS</li>
+                  <li>+10000 channels</li>
                   <li>99.9% Uptime</li>
                   </ul>
                   <br />
                   <h3 className="paypay">send to Paypal: med.benzbair@gmail.com</h3>
-                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-5 fa-paypal"></i>Pay Now</button></a>
+                  <a href="https://www.paypal.com/eg/webapps/mpp/send-money-online" target="_blank"><button className="btn btn-outline-warning"><i class="fab fa-paypal"></i>Pay Now</button></a>
 
                   </div>
                 </div>
               </div>
+            </div>  
             </div>
-
           </section>
 
-
           <section className="EMAIL">
-            <h1 className="EMAILUS">
-            📧 EMAIL US 📧
-            </h1>
-            <Login />
-
-
+            <div className="emaill">
+              <div className="container">
+                <h1 className="EMAILUS">
+                📧 EMAIL US 📧
+                </h1>
+                <Login />
           <section className="footer">
             <div className="copyright">
               Copyright &copy; 2020 <span>Welcome To IPTV</span>
-
             </div>
           </section>
 
+          </div>
+            </div>
           </section>
-
-
-
 
     </Router>
 
